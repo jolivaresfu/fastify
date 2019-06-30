071d8a2e32e63eab04e61aa579acfefb02bd6155
Juan Olivares <ext_jolivaresfu@sodimac.cl>
2019-06-30 10:43:28 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({ logger: true })
+
+fastify.register(require('./src/routes/first-route'));
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen(3000)
+        fastify.log.info(`server listening on ${fastify.server.address().port}`)
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start()
